86a34112d67f8b94634c3d2374e2e9b5198f4b70
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-04-12 20:14:03 +0300

    install sass make components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/images/blocks/style.scss";
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Body } from './components/Body/Body';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='page'>
+        <Header />
+        <div className='sidebar'>
+          <Sidebar />
+        </div>
+        <main className='main'>
+          <Body />
+        </main>
+      </div>
     </div>
   );
 }
