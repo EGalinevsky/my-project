935d05dd14913e26d1205a0538a180deca41be1e
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-04-13 19:49:05 +0300

    sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./assets/images/blocks/style.scss";
-import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Body } from './components/Body/Body';
 import { Sidebar } from './components/Sidebar/Sidebar';
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import sidebarHeader from "./../../assets/images/img_sidebar/sidebarHeader.jpg";
+import user from "./../../assets/images/img_sidebar/user.jpg";
+import instagram from "./../../assets/images/img_sidebar/instagram_1.svg";
+import vk from "./../../assets/images/img_sidebar/vk_1.svg";
+import pinterest from "./../../assets/images/img_sidebar/pinterest_1.svg";
 
-export const Sidebar =()=>{
-    return(
-        <div>
-           Sidebar 
+export const Sidebar = () => {
+  return (
+    <div>
+      <div className="sidebar_header">
+        <img src={sidebarHeader} alt="" />
+      </div>
+      <div className="sidebar_content">
+        <div className="profile">
+          <img className="avatar" src={user} alt="" />
+          <div className="profile_name">Eugene Galinevsky</div>
+          <div className="profile_prof">React-developer</div>
+          <ul className="social">
+            <li className="social_item">
+              <a className="social_link" href="#" target="_black">
+                <img src={instagram} alt="" />
+              </a>
+            </li>
+            <li className="social_item">
+              <a className="social_link" href="#" target="_black">
+                <img src={vk} alt="" />
+              </a>
+            </li>
+            <li className="social_item">
+              <a className="social_link" href="#" target="_black">
+                <img src={pinterest} alt="" />
+              </a>
+            </li>
+          </ul>
+          <div className="profile_text">
+            Front-end разработчик. Практик верстки сайтов. Созданием сайтов
+            занимаюсь с 2012 года. Работал в нескольких ИТ компаниях и наработал
+            более 10 000 часов в создании сайтов различной сложности.
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="sidebar_footer"></div>
+    </div>
+  );
+};
