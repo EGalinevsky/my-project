d15d8cdbfe75c8676e355b7ab1416e4d91b839a6
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-04-14 22:13:00 +0300

    header scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
-export const Header =()=>{
-    return(
-        <div>
-           hello 
-        </div>
-    )
-}
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__left">
+        <nav className="nav">
+          <ul>
+            <li>
+              <a className="nav__link has-subnav" href="#">
+                Main
+              </a>
+              <ul className="subnav">
+                <li>
+                  <a href="">creating websites</a>
+                </li>
+                <li>
+                  <a href="">Lololol websites</a>
+                </li>
+                <li>
+                  <a href="">lololoooolololos</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a className="nav__link" href="#">
+                Аrticle
+              </a>
+            </li>
+            <li>
+              <a className="nav__link" href="#">
+                About me
+              </a>
+            </li>
+            <li>
+              <a className="nav__link" href="#">
+                Аdvertisement
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="header__right">
+        <nav className="nav">
+          <ul>
+            <li>
+              <a className="nav__link" href="#">
+                Profile
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <form className="search" action="/" method="post">
+          <input placeholder="search" type="text" />
+        </form>
+      </div>
+    </header>
+  );
+};
