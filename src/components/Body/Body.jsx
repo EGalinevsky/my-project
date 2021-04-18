b9833aff4c0d3985793be34cb314cf46a1fe178c
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-04-18 20:20:46 +0300

    first page

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -4,7 +4,8 @@ import Rectangle_2 from "../../assets/images/img_sidebar/stories/Rectangle_2.jpg
 import Rectangle_3 from "../../assets/images/img_sidebar/stories/Rectangle_3.jpg";
 import Rectangle_4 from "../../assets/images/img_sidebar/stories/Rectangle_4.jpg";
 import send_btn from "../../assets/images/img_sidebar/send_btn.svg";
-import post2 from "../../assets/images/img_sidebar/Rectangle_5.jpg"
+import post2 from "../../assets/images/img_sidebar/Rectangle_5.jpg";
+import post3 from "../../assets/images/img_sidebar/Rectangle_6.jpg";
 
 export const Body = () => {
   return (
@@ -42,50 +43,150 @@ export const Body = () => {
       {/*stories ====== */}
       <div className="addPosts">
         <form className="add-post__form" action="/" method="post">
-          <textarea className='add-post__text' name="post-text" placeholder="Add post"></textarea>
+          <textarea
+            className="add-post__text"
+            name="post-text"
+            placeholder="Add post"
+          ></textarea>
           <div className="add-post__form-actions">
-            <label className='add-post__file' for='add-post-file'>
+            <label className="add-post__file" for="add-post-file">
               <input type="file" id="add-post-file" />
             </label>
-            <button className='add-post-send' type="submit">
-              <img src={send_btn} alt="send"/>
+            <button className="add-post-send" type="submit">
+              <img src={send_btn} alt="send" />
             </button>
           </div>
         </form>
       </div>
       {/*addPosts ==== */}
-      <div className='post'>
+      <div className="post">
         <div className="post__content">
           <p className="post__text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, porro maiores. Est sint cupiditate nihil! Inventore voluptatum illo rem, ut quis optio nisi praesentium beatae dicta exercitationem possimus doloribus natus.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, porro
+            maiores. Est sint cupiditate nihil! Inventore voluptatum illo rem,
+            ut quis optio nisi praesentium beatae dicta exercitationem possimus
+            doloribus natus.
           </p>
         </div>
         <div className="post__footer">
-            <ul className="post__data">
-              <li className='post__data-item'>
-                <time>21.06.2020</time>
-              </li>
-            </ul>
+          <ul className="post__data">
+            <li className="post__data-item">
+              <time>21.06.2020</time>
+            </li>
+          </ul>
         </div>
       </div>
       {/*/post/ ====== */}
-      <div className='post'>
+      <div className="post">
         <div className="post__header">
-          <img src={post2} alt=""/>
+          <img src={post2} alt="" />
         </div>
         <div className="post__content">
+          <h2 className="post__title">
+            <a href="#">How written cod quite and self</a>
+          </h2>
           <p className="post__text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, porro maiores. Est sint cupiditate nihil! Inventore voluptatum illo rem, ut quis optio nisi praesentium beatae dicta exercitationem possimus doloribus natus.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, porro
+            maiores. Est sint cupiditate nihil! Inventore voluptatum illo rem,
+            ut quis optio nisi praesentium beatae dicta exercitationem possimus
+            doloribus natus.
           </p>
         </div>
         <div className="post__footer">
-            <ul className="post__data">
-              <li className='post__data-item'>
-                <time>21.06.2020</time>
-              </li>
-            </ul>
+          <ul className="post__data">
+            <li className="post__data-item">
+              <time>21.06.2020</time>
+            </li>
+            <li className="post__data-item">
+              <a href="#">Make saitov</a>
+            </li>
+          </ul>
+          <a className="post__read" href="#">
+            Read
+          </a>
         </div>
       </div>
+      {/* post with img===== */}
+      <div className="post">
+        <div className="post__header"></div>
+        <div className="post__content">
+          <h2 className="post__title">
+            <a href="#">Bye new notebook from 150 000 $</a>
+          </h2>
+        </div>
+        <div className="post__footer">
+          <ul className="post__data">
+            <li className="post__data-item">
+              <time>21.06.2020</time>
+            </li>
+            <li className="post__data-item">
+              <a href="#">Продвижение видео</a>
+            </li>
+          </ul>
+          <a className="post__read" href="#">
+            Lief comment
+          </a>
+        </div>
+      </div>
+      {/* ===== */}
+      <div className="post">
+        <div className="post__header">
+          <img src={post3} alt="" />
+        </div>
+        <div className="post__content">
+          <h2 className="post__title">
+            <a href="#">Как я сходил на FrontEnd Conf 2021</a>
+          </h2>
+          <p className="post__text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, porro
+            maiores. Est sint cupiditate nihil! Inventore voluptatum illo rem,
+            ut quis optio nisi praesentium beatae dicta exercitationem possimus
+            doloribus natus.
+          </p>
+        </div>
+        <div className="post__footer">
+          <ul className="post__data">
+            <li className="post__data-item">
+              <time>21.06.2020</time>
+            </li>
+            <li className="post__data-item">
+              <a href="#">Make saitov</a>
+            </li>
+          </ul>
+          <a className="post__read" href="#">
+            Read
+          </a>
+        </div>
+      </div>
+      {/* ===== */}
+
+      <ul className="pagination">
+        <li className="pagination__item">
+          <a className="pagination__nav" href="#">
+            &lt;
+          </a>
+        </li>
+        <li className="pagination__item">
+          <a className="pagination__nav" href="">
+            1
+          </a>
+        </li>
+        <li className="pagination__item">
+          <a className="pagination__nav active" href="">
+            2
+          </a>
+        </li>
+        <li className="pagination__item">
+          <a className="pagination__nav" href="">
+            3
+          </a>
+        </li>
+        <li className="pagination__item">
+          <a className="pagination__nav" href="">
+            &gt;
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
